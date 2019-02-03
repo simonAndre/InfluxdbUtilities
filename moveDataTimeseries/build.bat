076bc4b03e2daac51138fa3fded139cb88e03d89
simon ANDRE <simon.andre@hagerservices.cloud>
2019-02-03 22:40:23 +0100

    organize field definitions

--- moveDataTimeseries/build.bat
+++ moveDataTimeseries/build.bat
@@ -1,1 +1,2 @@
-dotnet publish -c Release -r win10-x64
+dotnet publish -c Release -r win10-x64 --self-contained true
+set PATH=%PATH%;c:\sources\PrivateRepo\InfluxdbUtilities\moveDataTimeseries\bin\Release\netcoreapp2.1\win10-x64\publish\
--- moveDataTimeseries/csvdataloading.cs
+++ moveDataTimeseries/csvdataloading.cs
@@ -1,11 +1,11 @@
 ﻿using CsvHelper;
+using moveDataTimeseries.fieldsDefinition;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static moveDataTimeseries.Program;
 
 namespace moveDataTimeseries
 {
